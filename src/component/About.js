07b71293f5d5fc0c7a1704cdf5d5137994d776ae
Tanuja <tanujakhatun292@gmail.com>
2022-12-12 18:30:33 +0530

    Added dummy paragraph

--- src/component/About.js
+++ src/component/About.js
@@ -6,8 +6,9 @@ export default function About() {
     <div>
     <div className="">
     <div  className="container my-5 py-5 text-center">
-    <h1>Bring People  together <br></br>
+    <h1 className="my-4">Bring People  together
         With great food</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <button type="button"  className="btn btn-dark px-4 my-3">Let's talk</button>
     </div>
     </div> 
