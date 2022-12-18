fb8becadcc7b37e353557c7d79e9fe4ee5716763
Tanuja <tanujakhatun292@gmail.com>
2022-12-18 21:26:02 +0530

    added phone number field

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -7,6 +7,7 @@ export default function Contact() {
       <form className='text-center'>
         <input type="text" placeholder='Enter Your Name' className='w-50 p-2' /> <br></br>
         <input type="email" placeholder='Enter your email' className='my-3 w-50 p-2'/> <br></br>
+        <input type="text" placeholder='Enter your Phone Number' className='mb-3 w-50 p-2'/> <br></br>
         <button type="button" className="btn btn-warning px-5">SUBMIT</button>
       </form>
     </div>
