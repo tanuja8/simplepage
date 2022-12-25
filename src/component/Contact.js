b5f0e8e3a6d75d6c16cf0841a422ce6683354110
Tanuja <tanujakhatun292@gmail.com>
2022-12-25 23:49:24 +0530

    button colour changed

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
         <input type="text" placeholder='Enter Your Name' className='w-50 p-2' /> <br></br>
         <input type="email" placeholder='Enter your email' className='my-3 w-50 p-2'/> <br></br>
         <input type="text" placeholder='Enter your Phone Number' className='mb-3 w-50 p-2'/> <br></br>
-        <button type="button" className="btn btn-warning px-5">SUBMIT</button>
+        <button type="button" className="btn btn-danger px-5">SUBMIT</button>
       </form>
     </div>
   )
