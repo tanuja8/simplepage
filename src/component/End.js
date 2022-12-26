873368a3f1feafad8c9a91428fd2760e9c43e16b
Tanuja <tanujakhatun292@gmail.com>
2022-12-26 23:36:22 +0530

    fotter color changed

--- src/component/End.js
+++ src/component/End.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function End() {
   return (
-    <div className='py-4  text-center m-3'>
+    <div className='py-4 bg-dark text-center m-3 text-white'>
       <p className='display-6'>copyright@2022 || All right reserved by Tanuja</p>
     </div>
   )
