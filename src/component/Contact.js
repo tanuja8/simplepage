cfdf8fa0d99be956ea351a102cdc22ef51a50ded
Tanuja <tanujakhatun292@gmail.com>
2022-12-29 23:37:24 +0530

    added message box

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,12 +3,15 @@ import React from 'react'
 export default function Contact() {
   return (
     <div className='container mx-auto'>
-        <h1 className='text-center my-4'>Contact</h1>
+      <h1 className='text-center my-4'>Contact</h1>
       <form className='text-center'>
         <input type="text" placeholder='Enter Your Name' className='w-50 p-2' /> <br></br>
-        <input type="email" placeholder='Enter your email' className='my-3 w-50 p-2'/> <br></br>
-        <input type="text" placeholder='Enter your Phone Number' className='mb-3 w-50 p-2'/> <br></br>
-        <button type="button" className="btn btn-danger px-5">SUBMIT</button>
+        <input type="email" placeholder='Enter your email' className='my-3 w-50 p-2' /> <br></br>
+        <input type="text" placeholder='Enter your Phone Number' className='mb-3 w-50 p-2' /> <br></br>
+        <textarea id="freeform" name="freeform" rows="4" cols="50" className=''>
+          Type your Message...
+        </textarea><br/>
+        <button type="button" className="btn btn-danger px-5 m-5">SUBMIT</button>
       </form>
     </div>
   )
